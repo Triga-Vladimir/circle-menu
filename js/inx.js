142192fd15d8a8d53647a4832f32ed5b65f722b6
Triga <71762410+Triga777@users.noreply.github.com>
2021-10-22 23:55:37 +0300

    init project

--- js/inx.js
+++ js/inx.js
@@ -1,26 +1,28 @@
-const menuBtn = document.querySelector('[has-ripple="true"]');
-menuBtn.addEventListener('click', () => {
-    console.log(document.querySelector(this));
+const menuBtn = document.querySelector('[data-btn]');
+const menuItem = document.querySelectorAll('.menuitem-wrapper');
+const menuLinks = document.querySelectorAll('.menu a');
 
-    // document.querySelector(this).classList.toggle('clicked');
+menuBtn.addEventListener('click', (e) => {
+    menuBtn.classList.toggle('clicked');
     document.querySelector('.menu').classList.toggle('open');
 });
 
-//   document.querySelector('.menu a').each((index) => {
-//     const thismenuItem = document.querySelector(this);
-//     console.log(this);
-    
-//     thismenuItem.click(function (event) {
-//       event.preventDefault();
-      
-//       document.querySelector('.menuitem-wrapper').eq(index).classList.add('spin');
-      
-//       const timer = setTimeout(function () {
-//         document.querySelector('body').removeAttr('class').classList.add('bg-'+index);
-//         document.querySelector('.menuitem-wrapper').eq(index).removeClass('spin');
-//         document.querySelector('.menu').removeClass('open');
-//         document.querySelector('.menu-btn').removeClass('clicked');
-//       }, 800);
-//     });
-//   });
+menuItem.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    menuItem.forEach((btnItem, inx) =>{
+        if(btnItem === btn){
+        btnItem.classList.add('spin');
+
+        const timer = setTimeout(() => {
+          document.querySelector('body').removeAttribute('class');
+          document.querySelector('body').classList.add('bg-'+inx);
+          document.querySelector('.menu').classList.remove('open');
+          btnItem.classList.remove('spin');
+          menuBtn.classList.remove('clicked');
+        }, 570);
+      }
+    })
+  })
+});
